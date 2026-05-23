1bbf269928bbea1aa8c2434673e0c114113f2636
Cauã da Paz <cauadapaz2023@gmail.com>
2026-05-23 10:16:47 -0300

    criação da classe Empresa

---
+++ src/Model/Empresa.java
@@ -0,0 +1,53 @@
+package Model;
+
+public class Empresa {
+
+    private Long idEmpresa;
+    private String razaoSocial;
+    private String cnpj;
+    private String setor;
+    private String cidade;
+
+    public Empresa() {
+    }
+
+    public Long getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(Long idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
